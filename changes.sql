3d23350877122d11931428cccca2f1dc0a5ff2f1
Nkululeko-VillI Nhlapo <villicentnkululeko@gmail.com>
2024-06-10 04:12:04 +0000

    fix: Add UNIQUE and NOT NULL constraints to elements table

--- changes.sql
+++ changes.sql
@@ -5,3 +5,8 @@ ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
 Setting NOT NULL constraints on columns
 ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
 ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
+Adding UNIQUE and NOT NULL constraints to elements table
+ALTER TABLE elements ADD CONSTRAINT unique_symbol UNIQUE (symbol);
+ALTER TABLE elements ADD CONSTRAINT unique_name UNIQUE (name);
+ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;
+ALTER TABLE elements ALTER COLUMN name SET NOT NULL;
